0b181f32a340eae1d6f5a683a9d038e0e8b05fe3
Melinda Estrada <melinda_kane@yahoo.com>
2024-04-07 19:23:25 -0400

    Final run of dagster and README updates

--- dagster/analytics/assets/airbyte/airbyte.py
+++ dagster/analytics/assets/airbyte/airbyte.py
@@ -2,11 +2,9 @@ from dagster import EnvVar, AutoMaterializePolicy, FreshnessPolicy
 from dagster_airbyte import AirbyteResource, load_assets_from_airbyte_instance
 
 airbyte_resource = AirbyteResource(
-    #host="44.211.46.125",
     host=EnvVar("airbyte_host"),
     port="8000",
     username="airbyte",
-    #password="password",
     password=EnvVar("airbyte_password"),
 
 )
@@ -14,7 +12,7 @@ airbyte_resource = AirbyteResource(
 airbyte_assets = load_assets_from_airbyte_instance(
     airbyte_resource,
     key_prefix="northwind",
-    connection_filter=lambda meta: "2" in meta.name, #filters connections to not grab duplicate connection
+    connection_filter=lambda meta: "3" in meta.name, #filters connections to not grab duplicate connection
 
     )
 
